69575e19e5f1e279bb1dd8292eea847d91c1a8cb
ajodar <69302933+ajodar@users.noreply.github.com>
2021-01-15 10:46:14 +0100

    CaregorySeed

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -11,6 +12,29 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        /*REVISAR
+
+        $faker = Faker\Factory::create();
+
+        $category_names=['Music', 'Books', 'Theatre', 'Dance','Opera', 'Painting'];
+        $total=count($category_names);
+
+        foreach($category_names as $category_name){
+
+            $category = new Category;
+            $category->ImgIcon_id = $faker->numberBetween(0, $total) ; //Dona num de 0 a 10
+
+            $category->{'Name:es'} = $category_name;
+
+
+
+
+        }
+
+
+        */
+
+
     }
 }
--- database/seeds/Category_translationSeeder.php
+++ database/seeds/Category_translationSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 
 class Category_translationSeeder extends Seeder
@@ -11,6 +12,6 @@ class Category_translationSeeder extends Seeder
      */
     public function run()
     {
-        //
+       //
     }
 }
