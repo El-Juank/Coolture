f64c90c2b31ffaa31463b02e08583582ea9b1833
Gabriel Cortés <tetradog@gmail.com>
2021-01-15 13:30:14 +0100

    categories seeder working

--- app/CategoryTranslation.php
+++ app/CategoryTranslation.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Location_translation extends Model
+class CategoryTranslation extends Model
 {
-   use SoftDeletes;
+    use SoftDeletes;
 }
--- app/LocationTranslation.php
+++ app/LocationTranslation.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Notification_translation extends Model
+class LocationTranslation extends Model
 {
-    use SoftDeletes;
+   use SoftDeletes;
 }
