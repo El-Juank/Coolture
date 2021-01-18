9b4abe15e0eabc020620dd30ace0a252e61f2275
Gabriel Cortés <tetradog@gmail.com>
2021-01-18 19:57:43 +0100

    acabant RangeSeeder

--- database/seeds/RangeSeeder.php
+++ database/seeds/RangeSeeder.php
@@ -15,21 +15,27 @@ class RangeSeeder extends Seeder
      */
     public function run()
     {
-        $faker=Faker\Factory::create();
-        $locations=Location::get();
-        $events=Event::get();
+        $faker = Faker\Factory::create();
+        $locations = Location::get();
+        $events = Event::get();
 
-        $totalLocations=count($locations);
+        $totalLocations = count($locations) - 1;
 
-        foreach($events as $event){
-            if($faker->boolean()||$faker->boolean()){//posso dos per tenir més probabilitats de tenir 
-            $range=new Range();
-            $range->Event_id=$event->id;
-            $range->Location_id=$locations[$faker->numberBetween(0, $totalLocations)]->id;
-            $range->Range=$faker->randomFloat(2,1,1000000);
-            $range->save();
+        foreach ($events as $event) {
+            $dic=[];
+            for ($i = 0, $f = $faker->numberBetween(1, $totalLocations); $i < $f; $i++) {
+                if ($faker->boolean() || $faker->boolean()) { //posso dos per tenir més probabilitats de tenir 
+                    $range = new Range();
+                    $range->Event_id = $event->id;
+                    do{
+                        $idPos=$faker->numberBetween(0, $totalLocations);
+                    }while(array_key_exists($idPos,$dic));
+                    $dic[$idPos]=null;
+                    $range->Location_id = $locations[$idPos]->id;
+                    $range->Range = $faker->randomFloat(2, 1, 1000000);
+                    $range->save();
+                }
             }
         }
-        
     }
 }
