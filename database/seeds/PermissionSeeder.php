45688867019d4b65ca24d3385cc9f0c0f1348921
Gabriel Cortés <tetradog@gmail.com>
2021-01-18 10:48:57 +0100

    tractament de permissos

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Role;
+use App\Permission;
+
 class PermissionSeeder extends Seeder
 {
     /**
@@ -11,6 +15,23 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        
+        $faker= Faker\Factory::create();
+
+        $users=User::get();
+        $roles=Role::get();
+        $totalUsers=count($users)-1;
+        $totalRols=count($roles)-1;
+        for($i=0;$i<=$totalUsers;$i++){
+            for($j=0;$j<=$totalRols;$j++){
+                
+                if($faker->boolean()){
+                    $permission=new Permission();
+                    $permission->GrantedBy=$users[$faker->numberBetween(0,$totalUsers)]->id;
+                    $permission->Role_id=$roles[$j]->id;
+                    $permission->User_id=$users[$i]->id;
+                    $permission->save();
+                }
+            }
+        }
     }
 }
