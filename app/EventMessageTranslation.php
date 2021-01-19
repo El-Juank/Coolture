81dc8cbec7768dcf2b2943238fc366862ced1410
Gabriel Cortés <tetradog@gmail.com>
2021-01-19 10:31:48 +0100

    tot correcte seeder

--- app/EventMessageTranslation.php
+++ app/EventMessageTranslation.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventMessageTranslation extends Model
 {
+    protected $table="EventMessage_Translations";
     public function EventMessage(){
         return $this->belongsTo(EventMessage::class);
     }
--- app/RumourMessageTranslation.php
+++ app/RumourMessageTranslation.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RumourMessageTranslation extends Model
 {
+    protected $table="RumourMessage_Translations";
     public function RumourMessage(){
         return $this->belongsTo(RumourMessage::class);
     }
