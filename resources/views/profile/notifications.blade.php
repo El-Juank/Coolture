26c38d08941d1dffe35818968f16aa92ec4cda05
Gabriel Cortés <tetradog@gmail.com>
2021-01-25 00:35:34 +0100

    adaptat als canvis

--- resources/views/profile/notifications.blade.php
+++ resources/views/profile/notifications.blade.php
@@ -1,26 +1,26 @@
 #Notficacións Event <br/>
-<a href="{{route('eventNotification',['event'=>'tots'])}}">Seen</a> 
+<a href="{{route('eventNotification',['event'=>'all'])}}">Seen</a> 
 @forelse($user->NotificationChangesEvent(true) as $notification)
 <div>
-<a href="{{route('eventNotification',['event'=>$notification->Event->id])}}" >{{$notification->Event->Title}}</a>
+<a href="{{route('eventNotification',['event'=>$notification->Event->id])}}" >{{$notification->Event->GetTitle()}}</a>
 </div>
 @empty
 <div>No hi ha notifications d'events </div>
 @endforelse
 #Notficacións EventMaker <br/>
-<a href="{{route('eventMakerNotification',['eventmaker'=>'tots'])}}">Seen</a> 
+<a href="{{route('eventMakerNotification',['eventmaker'=>'all'])}}">Seen</a> 
 @forelse($user->NotificationChangesEventMaker(true) as $notification)
 <div>
-<a href="{{route('eventMakerNotification',['eventmaker'=>$notification->EventMaker->id])}}" >{{$notification->EventMaker->Name}}</a>
+<a href="{{route('eventMakerNotification',['eventmaker'=>$notification->EventMaker->id])}}" >{{$notification->EventMaker->GetName()}}</a>
 </div>
 @empty
 <div>No hi ha notifications d'event makers </div>
 @endforelse
 #Notficacións Rumors <br/>
-<a href="{{route('rumourNotification',['rumour'=>'tots'])}}">Seen</a> 
+<a href="{{route('rumourNotification',['rumour'=>'all'])}}">Seen</a> 
 @forelse($user->NotificationChangesRumour(true) as $notification)
 <div>
-<a  href="{{route('rumourNotification',['rumour'=>$notification->Rumour->id])}}" >{{$notification->Rumour->Title}}</a>
+<a  href="{{route('rumourNotification',['rumour'=>$notification->Rumour->id])}}" >{{$notification->Rumour->GetTitle()}}</a>
 </div>
 @empty
 <div>No hi ha notifications de rumors </div>
