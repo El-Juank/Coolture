c044fc204596f0828f09d26b7b47c0726c169242
Gabriel Cortés <tetradog@gmail.com>
2021-01-27 21:22:31 +0100

    ja funciona :D

--- database/seeds/UserRangeSeeder.php
+++ database/seeds/UserRangeSeeder.php
@@ -13,25 +13,35 @@ class UserRangeSeeder extends Seeder
      *
      * @return void
      */
+    const MAXBUCLE=100;
     public function run()
     {
         $faker= Faker\Factory::create();
         $users=User::get();
         $eventMakers=EventMaker::get();
-
+        
         $totalEventMakers=count($eventMakers)-1;
+    
         foreach($users as $user){
             $dic=[];
-            for($i=0,$f=$faker->numberBetween(0,$totalEventMakers);$i<$f;$i++){
+            $total=0;
+            foreach(UserRange::where('user_id',$user->id)->get() as $uRange){
+                $dic[$uRange->event_maker_id]=null;
+            }
+            for($i=0,$f=$faker->numberBetween(0,3);$i<$f;$i++){
 
                 $userRange=new UserRange();
                 $userRange->user_id=$user->id;
                 do{
                     $eventMaker=$eventMakers[$faker->numberBetween(0,$totalEventMakers)];
-                }while(UserRange::where('user_id',$user->id)->where('event_maker_id',$eventMaker->id)->count()!=0);
+                    $total++;
+                }while(array_key_exists($eventMaker->id,$dic) && $total<self::MAXBUCLE );
+                if($total<self::MAXBUCLE){
+                $dic[$eventMaker->id]=null;
                 $userRange->event_maker_id=$eventMaker->id;
                 $userRange->Range=$faker->randomFloat(2,1,1000000);
                 $userRange->save();
+                }
 
 
             }
